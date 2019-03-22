2685f75305e6e025c1c1f408da61f7cd7cb3905d
8_n3z <jesus.saavedra37926637@gmail.com>
2019-03-22 16:45:16 -0700

    Reading the docs to see best way for page nav

---
+++ Firebase/src/pages/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "#000" }}>The return of billy</h1>
+      </div>
+    )
+  }
+}
+
+export default Test
--- Firebase/src/pages/index.js
+++ Firebase/src/pages/index.js
@@ -1,3 +1,11 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import "tachyons"
+
+// import RouterBoi from "./RouterBoi"
+
+// ReactDOM.render(<RouterBoi />, document.getElementById("root"))
+
 import React from "react"
 
 export default () => <div>Hello world!</div>
